50b3cfd63c773bfbf157ef50233cea342dc67732
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-06 02:21:30 +0300

    cx

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Define a function 'copy_list' that takes a list 'l' as input.
+# Use the slicing notation 'l[:]' to create and return a copy of the input list.
+def copy_list(l):
+    return l[:]
