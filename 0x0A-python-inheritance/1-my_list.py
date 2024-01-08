1042c9c16983f9edd261c34419cad3281610fbcb
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-08 20:57:53 +0300

    fr

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list in ascending order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
-
-# Example usage:
-my_list = MyList([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
-my_list.print_sorted()
+        print(sorted(self))
