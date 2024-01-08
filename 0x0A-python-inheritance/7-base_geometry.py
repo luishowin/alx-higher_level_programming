7666d48818493512037bbd75856560138c4ba0af
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-08 22:32:58 +0300

    jki

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,8 +1,11 @@
 #!/usr/bin/pythhon3
 """Defines a class BaseGeometry"""
 
+
 class BaseGeometry:
     """Defines a class BaseGeometry."""
+
+    
     def area(self):
         """Public instance method that raises an Exception.
 
