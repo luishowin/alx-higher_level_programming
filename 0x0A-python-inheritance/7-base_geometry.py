7e9dd89042212d5a2a6c4b676ace5d63da0fd412
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-08 22:25:02 +0300

    ik

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/pythhon3
+"""Defines a class BaseGeometry"""
+
+class BaseGeometry:
+    def area(self):
+        """Public instance method that raises an Exception.
+
+        Raises:
+        - Exception: area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method that validates an integer value.
+
+        Args:
+        - name (str): The name of the value.
+        - value: The value to be validated.
+
+        Raises:
+        - TypeError: If the value is not an integer.
+        - ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
