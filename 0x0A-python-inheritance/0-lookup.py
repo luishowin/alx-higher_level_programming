ceff4660b3701df13106802a8d38652c2c853f3d
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-08 21:53:54 +0300

    op

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Defines a class."""
+
+
 def lookup(obj):
-    """
-    Returns a list of available attributes and methods of an object.
+    """Returns a list of available attributes and methods of an object.
 
     :param obj: The object to inspect.
     :return: A list of attributes and methods.
