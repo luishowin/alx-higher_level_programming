728a055a02c6fded9d0e086c261b399dd342970c
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-08 22:07:19 +0300

    frt

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
-"""Defines a class."""
+"""Defines an obj attribute lookup func."""
 
 
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object.
-
-    :param obj: The object to inspect.
-    :return: A list of attributes and methods.
-    """
+    """Returns a list of available attributes and methods of an object."""
     return dir(obj)
-
-# Example usage:
-class MyClass1(object):
-    """Docstring: description of the class"""
-    pass
-
-class MyClass2(object):
-    """Docstring: description of the class"""
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
