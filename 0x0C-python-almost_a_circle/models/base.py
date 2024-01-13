96fabc3f07332ab06fae2d01d6d1790dbb05608d
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-13 10:54:04 +0300

    yeet

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class Base."""
+
+
+class Base:
+    """Private class attribute."""
+    __nb_objects = 0
+
+    """Class constructor."""
+    def __init__(self, id=None):
+        """
+        If id is not None, assign it to the public instance attribute id
+        Otherwise, increment __nb_objects and assign the new value to id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
